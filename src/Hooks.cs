64aa06e9a6010dbc23a23ca3e40579ac214f9236
Contiinuum <68207985+Contiinuum@users.noreply.github.com>
2020-07-20 09:55:38 +0200

    initial commit

---
+++ src/Hooks.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using MelonLoader;
+using UnityEngine;
+using Hmx.Audio;
+
+namespace AudicaModding
+{
+    internal static class Hooks
+    {
+        public static void ApplyHooks(HarmonyInstance instance)
+        {
+            instance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        /*[HarmonyPatch(typeof(PauseHandler), "HMDMounted")]
+        private static class PatchHMDMounted
+        {
+            private static void Postfix()
+            {
+                if (!PlayerPreferences.I.SpectatorCam.mVal || AudicaMod.spectatorCamFixed) return;
+                MelonCoroutines.Start(AudicaMod.FixSpectatorCam());
+            }
+        }*/
+
+        [HarmonyPatch(typeof(MenuState), "SetState")]
+        private static class PatchSetState
+        {
+            private static void Prefix(ref MenuState.State state)
+            {
+                if (AudicaMod.spectatorCamFixed) return;
+                if(state == MenuState.State.MainPage)
+                {
+                    AudicaMod.FixSpectatorCam();
+                }
+            }
+        }
+
+    }
+}
---
+++ src/Main.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using MelonLoader;
+using UnityEngine;
+using Harmony;
+using System.Collections;
+
+namespace AudicaModding
+{
+    public class AudicaMod : MelonMod
+    {
+
+        static public bool spectatorCamFixed = false;
+
+        public static class BuildInfo
+        {
+            public const string Name = "SpectatorCamFix";  // Name of the Mod.  (MUST BE SET)
+            public const string Author = "Continuum"; // Author of the Mod.  (Set as null if none)
+            public const string Company = null; // Company that made the Mod.  (Set as null if none)
+            public const string Version = "0.1.0"; // Version of the Mod.  (MUST BE SET)
+            public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+        }
+
+        public override void OnApplicationStart()
+        {
+            HarmonyInstance instance = HarmonyInstance.Create("AudicaMod");
+            Hooks.ApplyHooks(instance);
+        }       
+
+        public static void FixSpectatorCam()
+        {           
+            //yield return new WaitForSeconds(.5f);
+            spectatorCamFixed = true;
+            SpectatorCam cam = GameObject.FindObjectOfType<SpectatorCam>();
+            cam.cam.enabled = false;
+            cam.cam.enabled = true;
+            
+        }
+       
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
