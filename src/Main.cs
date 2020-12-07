fb89d0bbcb7a760d3c85a0102aec4d7d882a841e
Contiinuum <68207985+Contiinuum@users.noreply.github.com>
2020-12-07 19:30:26 +0100

    version bump

--- src/Main.cs
+++ src/Main.cs
@@ -17,14 +17,13 @@ namespace AudicaModding
             public const string Name = "SpectatorCamFix";  // Name of the Mod.  (MUST BE SET)
             public const string Author = "Continuum"; // Author of the Mod.  (Set as null if none)
             public const string Company = null; // Company that made the Mod.  (Set as null if none)
-            public const string Version = "0.1.0"; // Version of the Mod.  (MUST BE SET)
+            public const string Version = "1.0.1"; // Version of the Mod.  (MUST BE SET)
             public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
         }
 
         public override void OnApplicationStart()
         {
-            HarmonyInstance instance = HarmonyInstance.Create("AudicaMod");
-            Hooks.ApplyHooks(instance);
+            HarmonyInstance instance = HarmonyInstance.Create("SpectatorCamFix");
         }       
 
         public static void FixSpectatorCam()
@@ -35,7 +34,7 @@ namespace AudicaModding
 
             if(cam is null)
             {
-                MelonModLogger.Log("Spectator cam is disabled.");
+                MelonLogger.Log("Spectator cam is disabled.");
                 return;
             }
 
